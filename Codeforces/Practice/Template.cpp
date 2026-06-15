e0cfe20012ae691a6e08677378de5028a446b3a5
signull <tonkaopremprabha@gmail.com>
2026-06-15 20:43:38 +0700

    Update Template.cpp

--- Codeforces/Practice/Template.cpp
+++ Codeforces/Practice/Template.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define ll long long
+#define endl '\n'
+#define vin(v) for (auto &x : v) cin >> x
+#define vout(v) for (auto &x : v) cout << x << ' '; cout << endl
 
 void Solve() {
 
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    fastIO;
 
     ll testCount;
     cin >> testCount;
